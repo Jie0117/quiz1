8769509c3a76138d25ea260271977df46a56a1f4
Jie0117 <59718431+Jie0117@users.noreply.github.com>
2021-05-05 16:51:08 +0800

    Update quiz1-3.js

--- quiz1-3.js
+++ quiz1-3.js
@@ -1,14 +1,14 @@
-function change() { 
-            var header=$("hid123")[0];     
-            header.innerHTML = "CSIE@CGU";
-            var para=$("pid123")[0];
-                
-            para.innerHTML = "怎麼那麼棒！！.";   
-        }   
-            function myFunction() 
-            {   
-                 var btn = document.createElement("BUTTON");   
-                 btn.innerHTML = "Change this document."; 
-                 document.body.appendChild(btn).addEventListener("click", change); 
-                 
-            }
+
+function change() {
+    $("#aa").html("CSIE@CGU");
+    $("#bb").html("怎麼那麼棒")
+    
+}
+
+function new_button() {
+    var btn="<button id='change'>Change this document</button>";
+    $("body").append(btn);
+    $("#change").click(change);
+    
+}
+$("#btn").click(new_button);
